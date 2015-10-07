148bf812cefa3c0d0f983bf418495dd72cdfaf5a
Bryan <bryanjappleby@gmail.com>
2015-10-08 01:43:39 +0300

    Use Context

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context
 
 from .models import Greeting
 
+from fetch import Fetch
+
+
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+    c = Context({
+		"mine":"a",
+		"theirs":"b"
+	})
+    return render(request, 'index.html', c)
 
 def result(request):
     # return HttpResponse('Hello from Python!')
