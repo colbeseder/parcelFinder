f654f88cf526f61e1a1c9209cf6f66f375b43649
Bryan <bryanjappleby@gmail.com>
2015-10-08 01:28:40 +0300

    Start using Fetch interface

--- hello/fetch.py
+++ hello/fetch.py
@@ -1,6 +1,14 @@
 class Fetch:
+	example1 = "console.log('ex1<p>');"
+	example2 = "console.log('ex2<p>');"
 	def __init__(self, player):
 		self.player = player
 	
 	def whoami(self):
 		return self.player
+
+	def mine(self, escaped=True):
+		return self.example1
+
+	def theirs(self):
+		return self.example2
